8be96bca0cbf0b5526b5125fd3b157b2b3fd1375
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-03-10 14:39:54 +0000

    CCM-8840 S3 Access Logging

--- infrastructure/terraform/components/acct/outputs.tf
+++ infrastructure/terraform/components/acct/outputs.tf
@@ -8,6 +8,11 @@ output "dns_zone" {
 
 output "s3_buckets" {
   value = {
+    access_logs = {
+      arn    = module.s3bucket_access_logs.arn
+      bucket = module.s3bucket_access_logs.bucket
+      id     = module.s3bucket_access_logs.id
+    }
     lambda_function_artefacts = {
       arn    = module.s3bucket_lambda_artefacts.arn
       bucket = module.s3bucket_lambda_artefacts.bucket
