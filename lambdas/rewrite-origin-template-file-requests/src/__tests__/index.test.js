7e1608b88a5cf2af119fb476ba5426ed43607452
Michael Harrison <michael.harrison@bjss.com>
2025-05-27 11:04:13 +0100

    Revert "CCM-9875: use cloudfront function over lambda"
    This reverts commit 07c7de2f4b926651be92d6779752d1d5c22dbaf2.

--- lambdas/rewrite-origin-template-file-requests/src/__tests__/index.test.js
+++ lambdas/rewrite-origin-template-file-requests/src/__tests__/index.test.js
@@ -2,15 +2,23 @@ const { handler } = require("../index");
 
 describe("template file download origin request rewrite", () => {
   test("it strips the leading /templates/files from the request uri forwarded to the origin", () => {
+    const cb = jest.fn();
     const event = {
-      request: {
-        uri: "/templates/files/owner-id/example.txt",
-      },
+      Records: [
+        {
+          cf: {
+            request: {
+              uri: "/templates/files/owner-id/example.txt",
+            },
+          },
+        },
+      ],
     };
 
-    const response = handler(event);
+    handler(event, {}, cb);
 
-    expect(response).toEqual(
+    expect(cb).toHaveBeenCalledWith(
+      null,
       expect.objectContaining({
         uri: "/owner-id/example.txt",
       })
@@ -18,15 +26,23 @@ describe("template file download origin request rewrite", () => {
   });
 
   test("it does not modify requests where the uri does not match", () => {
+    const cb = jest.fn();
     const event = {
-      request: {
-        uri: "/foo/bar/owner-id/example.txt",
-      },
+      Records: [
+        {
+          cf: {
+            request: {
+              uri: "/foo/bar/owner-id/example.txt",
+            },
+          },
+        },
+      ],
     };
 
-    const response = handler(event);
+    handler(event, {}, cb);
 
-    expect(response).toEqual(
+    expect(cb).toHaveBeenCalledWith(
+      null,
       expect.objectContaining({
         uri: "/foo/bar/owner-id/example.txt",
       })
